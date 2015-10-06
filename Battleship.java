7fa34f1747eae5b5dd09cd31d001cd65aeffe0bd
keldon <keldon.fischer@gmail.com>
2015-10-05 22:10:55 -0700

    Allow users to add and locate their ships

--- Battleship.java
+++ Battleship.java
@@ -1,16 +1,25 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Battleship {
     private static int MAX_SHIPS = 5;
 
+    
+    
+    
     public static void main(String[] args) {
         BattleField playerField = new BattleField(10);
+for (int ms = 1; ms <= MAX_SHIPS; ms++) {
+        System.out.println("Get ready to place your ships.");
+        System.out.println("You get 5 ships pick wisely");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter coordinates for Ship 1:"); 
+        String coordinates = keyboard.nextLine();
 
-        playerField.addShip(1, 1);
-        playerField.addShip(0, 3);
-        playerField.addShip(9, 3);
-        playerField.addShip(2, 1);
-
+        Point shipLocation = getPoint(coordinates);
+        playerField.addShip(shipLocation.getX(),
+            shipLocation.getY());
+}
         System.out.println("Player's Field:");
         FieldDisplay playerDisplay = new FieldDisplay(playerField, true);
         playerDisplay.render();
@@ -24,6 +33,23 @@ public class Battleship {
         FieldDisplay computerDisplay = new FieldDisplay(computerField, showComputerShips);
         computerDisplay.render();
 
+    }
+
+    private static Point getPoint(String coordinate) {
+        coordinate = coordinate.toUpperCase();
+
+        if (coordinate.length() == 2) {
+            char letter = coordinate.charAt(0);
+            char number = coordinate.charAt(1);
+        
+            int x = (letter - 'A');
+            int y = Character.getNumericValue(number) - 1;
+
+            return new Point(x, y);
+        }
+        else {
+            return Point.INVALID_POINT;
+        }
     }
 
 }
